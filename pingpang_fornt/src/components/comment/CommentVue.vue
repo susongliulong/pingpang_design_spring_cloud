155bab417fd806ab0663d652ac7200c8bf97c5ee
pgzlhq <susongliulong@163.com>
2024-03-24 18:41:28 +0800

    晚上评论组件(CommentVue)设计

--- pingpang_fornt/src/components/comment/CommentVue.vue
+++ pingpang_fornt/src/components/comment/CommentVue.vue
@@ -1,12 +1,34 @@
 <template>
+  <el-row>
+    <el-col :span="2">
+      <img src="" alt="照片" style="width: 60px; height: 60px" />
+    </el-col>
+    <el-col :offset="1" :span="10">
+      <el-input
+        v-model="commentContent"
+        placeholder="良言一句三冬暖"
+        size="normal"
+        @change=""
+      >
+      </el-input>
+    </el-col>
+    <el-col :span="2" :offset="1">
+      <el-button type="primary" size="default" @click="">发布</el-button>
+    </el-col>
+  </el-row>
+
   <div class="comment" v-for="(comment, index) in comments" :index="index">
     <div id="userMessage" class="clear">
       <div class="avator left">
         <img src="" alt="" style="width: 60px; height: 60px" />
       </div>
       <div class="content left">
-        <p>{{ comment.user.name }} 积分{{ comment.user.points }}</p>
-        <p>{{ comment.content }}</p>
+        <p>
+          <span style="font-size: bold">{{ comment.user.name }} </span>
+          <span style="color: red">积分&nbsp;&nbsp;</span
+          >{{ comment.user.points }}
+        </p>
+        <p style="font-size: 14px">{{ comment.content }}</p>
         <div class="helperMessage">
           <span>{{ comment.publishTime }}</span>
           <span
@@ -15,10 +37,18 @@
           <span
             ><el-con> <Dislike /> </el-con> {{ comment.dislikes }}</span
           >
-          <span @click="showReplyWindow = !showReplyWindow">回复</span>
+          <span
+            class="reply"
+            @click="
+              showReplyWindow = !showReplyWindow;
+              comment.isReplyWindowOpen = !comment.isReplyWindowOpen;
+            "
+            >&nbsp;&nbsp;回复</span
+          >
         </div>
       </div>
     </div>
+    <!-- 其他人回复user的评论 -->
     <div id="mainBody">
       <div
         class="subComment clear"
@@ -30,12 +60,18 @@
         </div>
         <div class="content left">
           <div>
-            {{ subComment.user.name }} 拥有积分{{ comment.user.points }}
+            <span style="font-size: bold">{{ subComment.user.name }} </span>
+            <span style="color: red">积分&nbsp;</span
+            >{{ subComment.user.points }}
+            <span>&nbsp;&nbsp;&nbsp;</span>
             <span v-if="subComment.replyToId !== comment.user.id"
-              >回复<span>@ {{ subComment.replyToName }}</span>
-              <span>{{ subComment.content }}</span></span
+              ><span style="font-size: 12px; color: red">回复&nbsp;&nbsp;</span
+              ><span>@{{ subComment.replyToName }}&nbsp;&nbsp;</span>
+              <span style="font-size: 12px">{{
+                subComment.content
+              }}</span></span
             >
-            <span v-else>{{ subComment.content }}</span>
+            <span style="font-size: 12px" v-else>{{ subComment.content }}</span>
           </div>
           <div class="helperMessage">
             <span>{{ comment.publishTime }}</span>
@@ -45,21 +81,69 @@
             <span
               ><el-con> <Dislike /> </el-con> {{ comment.dislikes }}</span
             >
-            <span @click="showReplyWindow = !showReplyWindow">回复</span>
+            <span
+              class="reply"
+              @click="
+                showReplyWindow = !showReplyWindow;
+                comment.isReplyWindowOpen = !comment.isReplyWindowOpen;
+              "
+              >&nbsp;&nbsp;回复</span
+            >
           </div>
         </div>
       </div>
-      <div id="footer">共79条评论，点击查看</div>
-      <div v-if="showReplyWindow" id="writeComment">
-        <img src="" alt="" style="width: 40px; height: 40px" />
-        <el-input
-          v-model="commentContent"
-          :placeholder="'回复' + comment.user.name"
-          size="normal"
-          @change=""
+      <!-- 查看更多的评论 -->
+      <div id="footer">
+        <span v-if="comment.number >= 10 && hidePagination">
+          共<span>{{ comment.number }}</span
+          >条评论，<span id="watchMore" @click="hidePagination = false"
+            >点击查看</span
+          ></span
         >
-        </el-input>
-        <el-button type="primary" size="default" @click="">发布</el-button>
+        <div v-if="!hidePagination" class="comment-pagination">
+          <el-row>
+            <el-col :span="13">
+              <el-pagination
+                v-model:current-page="currentPage"
+                :page-size="pageSize"
+                :small="false"
+                :background="true"
+                layout="total, prev, pager, next"
+                :total="comment.number"
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+              />
+            </el-col>
+
+            <el-col :span="1" :offset="1">
+              <el-button type="text" size="small" @click="hidePagination = true"
+                style="margin-top: 5px">收起评论</el-button
+              >
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <div
+        v-if="showReplyWindow && comment.isReplyWindowOpen"
+        id="writeComment"
+      >
+        <el-row>
+          <el-col :span="2">
+            <img src="" alt="照片" style="width: 60px; height: 60px" />
+          </el-col>
+          <el-col :offset="1" :span="10">
+            <el-input
+              v-model="commentContent"
+              placeholder="良言一句三冬暖"
+              size="normal"
+              @change=""
+            >
+            </el-input>
+          </el-col>
+          <el-col :span="2" :offset="1">
+            <el-button type="primary" size="default" @click="">发布</el-button>
+          </el-col>
+        </el-row>
       </div>
     </div>
   </div>
@@ -75,6 +159,8 @@ interface User {
 
 interface Comment {
   user: User; // 评论发布者
+  number: number; // 该评论下包含子评论的数量
+  isReplyWindowOpen: Boolean;
   replyToId: bigint;
   replyToName: string;
   id: bigint;
@@ -84,14 +170,22 @@ interface Comment {
   dislikes: number;
   comments: Comment[]; // 嵌套评论
 }
-
+// 显示回复窗口
 const showReplyWindow = ref<Boolean>(false);
+
+// 分页条相关属性
+const hidePagination = ref(true);
+const currentPage = ref<number>(5);
+const pageSize = ref(10); // 每一页中包含评论的数量
+const total = ref(1000);
 
 const user = ref<User>({ id: 1234, name: "追风", avator: "hdh", points: 12 });
 
 const comments = ref<Comment[]>([
   {
     user: { id: 1234, name: "追风", avator: "hdh", points: 12 },
+    number: 100,
+    isReplyWindowOpen: false,
     id: 123,
     replyToId: 0,
     replyToName: "",
@@ -102,6 +196,8 @@ const comments = ref<Comment[]>([
     comments: [
       {
         user: { id: 1234, name: "追风", avator: "hdh", points: 12 },
+        number: 100,
+        isReplyWindowOpen: false,
         replyToId: 1234,
         replyToName: "追风",
         id: 123,
@@ -112,6 +208,8 @@ const comments = ref<Comment[]>([
       },
       {
         user: { id: 1235, name: "追风", avator: "hdh", points: 12 },
+        number: 100,
+        isReplyWindowOpen: false,
         replyToId: 1235,
         replyToName: "追风",
         id: 123,
@@ -124,6 +222,8 @@ const comments = ref<Comment[]>([
   },
   {
     user: { id: 1235, name: "追风", avator: "hdh", points: 12 },
+    number: 150,
+    isReplyWindowOpen: false,
     id: 123,
     content: "哥笑了，但不白笑，祝高三学子高考顺利，金榜题名！",
     publishTime: "2024-12-23 14:57:00",
@@ -131,7 +231,51 @@ const comments = ref<Comment[]>([
     dislikes: 1234,
   },
 ]);
+
+// 评论分页条相关方法
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
 </script>
 <style scoped>
+.comment-pagination{
+  margin-top: 20px;
+}
 
+.comment {
+  background-color: white;
+  margin-top: 20px;
+  padding: 10px;
+  border-bottom: 2px solid red;
+}
+.content {
+  margin-left: 20px;
+}
+.subComment {
+  margin-left: 20px;
+  margin-top: 20px;
+}
+.helperMessage {
+  font-size: 12px;
+  color: #9499a0;
+}
+.helperMessage .reply:hover {
+  color: #2e58ff;
+  cursor: pointer;
+}
+#footer {
+  font-size: 12px;
+  color: #9499a0;
+}
+
+#footer #watchMore:hover {
+  color: #2e58ff;
+  cursor: pointer;
+}
+#writeComment {
+  margin-top: 20px;
+}
 </style>
